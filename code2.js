310f024756545c30b98e41eb882bfae8a41ce208
rosekamau <kamaurose236@gmail.com>
2024-07-02 00:32:06 +0300

    challenge

--- code2.js
+++ code2.js
@@ -1,14 +1,15 @@
-let gSalary =("bsalary"+"benefits")
+let bsalary =prompt("Enter your bSalary");
+let bebefits = prompt("Enter your benefits")
+let gSalary =(bsalary +benefits)
 console.log("gSalary")
 
 let bSalary= 80000;
 let benefits = 5000;
 
-let salary = (gSalary("bsalary"+"benefits"))
+let salary = (gSalary(bsalary+ benefits))
 console.log("salary")
 
 let nhifrate =0.030;
-let deductions =(gSalary*nhifrate);
+let deductions =(gSalary * nhifrate);
 console.log("deductions")
 
-let 
